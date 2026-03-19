33e042bf27a21fdf8b42cd94d192ea58e0d9a24e
tylerJordan223 <tjlinkmaster22@gmail.com>
2026-03-19 14:56:39 -0400

    Ski rotation nearly working

--- UntitledSkiGame/Assets/ProjectAssets/Scripts/Player/Cameras/SkiCamera.cs
+++ UntitledSkiGame/Assets/ProjectAssets/Scripts/Player/Cameras/SkiCamera.cs
@@ -1,6 +1,9 @@
+using Autodesk.Fbx;
 using Global_Input;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.Rendering.LookDev;
 
 public class SkiCamera : MonoBehaviour
 {
@@ -9,9 +12,10 @@ public class SkiCamera : MonoBehaviour
     public Transform player;
     public Transform player_obj;
     public Rigidbody rb;
+    public GameObject skis;
 
     [Range(0f, 100f)]
-    public float rotation_speed = 50f;
+    public float rotation_speed = 20f;
 
     //input
     private GlobalInput input;
@@ -40,6 +44,39 @@ public class SkiCamera : MonoBehaviour
         if(rotation != 0)
         {
             player.Rotate(0, rotation * rotation_speed * Time.deltaTime, 0);
+
+            //handle ski rotation
+            foreach(Transform ski in skis.transform.GetComponentsInChildren<Transform>())
+            {
+                if(rotation < 0)
+                {
+                    //turning left
+                    if(ski.rotation.y > -0.20)
+                    {
+                        ski.Rotate(0, rotation * rotation_speed * Time.deltaTime, 0f, Space.Self);
+                    }
+                }
+                else 
+                {
+                    //turning right
+                    if (ski.rotation.y < 0.20)
+                    {
+                        ski.Rotate(0, rotation * rotation_speed * Time.deltaTime, 0f, Space.Self);
+                    }
+                }
+            }
+        }
+        else
+        {
+            //straighten out skis if necessary
+            foreach (Transform ski in skis.transform.GetComponentsInChildren<Transform>())
+            {
+                //straighten out the kis by multiplying them by the negative of hteir current rotation
+                if (Mathf.Abs(ski.localRotation.y) > 0)
+                {
+                    ski.Rotate(0, 15f * -Mathf.Sign(ski.rotation.y) * Time.deltaTime, 0, Space.Self);
+                }
+            }
         }
     }
 }
